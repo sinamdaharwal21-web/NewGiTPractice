523f9b68d2faa799c6bcb8a66e2f04c1f709c297
sinamdaharwal21-web <sinamdaharwal21@gmail.com>
2025-10-17 15:41:15 +0530

    simple code added

---
+++ Example.java
@@ -0,0 +1,7 @@
+package com.sample.git.practice;
+
+public class Example {
+public static void main(String[] args) {
+	System.out.println("hhhhhhhhhhhhhhhhhhhh");
+}
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.sample.git.practice;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		System.out.println(i);
+	}
+	}
+}
