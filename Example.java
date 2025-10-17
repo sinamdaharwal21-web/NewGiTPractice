4f949427382eea631175953415a2a4c2749d8e30
sinamdaharwal21-web <sinamdaharwal21@gmail.com>
2025-10-17 15:54:09 +0530

    add brother name

--- Example.java
+++ Example.java
@@ -3,5 +3,7 @@ package com.sample.git.practice;
 public class Example {
 public static void main(String[] args) {
 	System.out.println("hhhhhhhhhhhhhhhhhhhh");
+	System.out.println("Sinam");
+	System.out.println("nitanshu");
 }
 }
